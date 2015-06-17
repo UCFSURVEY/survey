39339beba4b14efa73f473abdbc709ce2b7b010b
UCFSURVEY <marckensoncivil@knights.ucf.edu>
2015-06-16 19:43:23 -0700

    Delete ddl.sql

--- ddl.sql
+++
@@ -1,3 +0,0 @@
-create table answers (user text,sid text,qnum text, question text, answer text);
-create table survey (user text,sid text,qnum text, ques text, ans1 text, ans2 text, ans3 text, ans4 text);
-create table user_accts (username text,password text);
